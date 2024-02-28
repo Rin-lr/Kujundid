3519ee0bfe34d6f16763568e9fc7d2b7f984c4f5
Karina Reisel <kreise@taltech.ee>
2024-02-28 16:44:45 +0200

    kolmnurk valmis

--- Kolmnurk.vb
+++ Kolmnurk.vb
@@ -2,11 +2,20 @@
 Public Class Kolmnurk
     Inherits Kujund
 
+    Private korgus As Double
+
+    Public Sub New(ByVal alus As Double, ByVal haar As Double)
+        'kutsume baasklassi'
+        MyBase.New(("Võrdhaarne kolmnurk"), alus, haar, haar))
+            'kolmnurga kõrgus'
+            Me.korgus = Sqrt(haar * haar - (alus * alus / 4))
+    End Sub
+
     Public Overrides Function leiaYmberMoot() As Double
-        Throw New NotImplementedException()
+        Return kylgA + kylgB + kylgC
     End Function
 
     Public Overrides Function leiaPindala() As Double
-        Throw New NotImplementedException()
+        Return kylgA * korgus / 2
     End Function
 End Class
