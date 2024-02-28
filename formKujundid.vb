2d2662cc31219dc6badecb78406045e3359216b3
Karina Reisel <kreise@taltech.ee>
2024-02-28 17:29:42 +0200

    kuni nupude func tehtud.

--- formKujundid.vb
+++ formKujundid.vb
@@ -1,9 +1,26 @@
 ﻿Public Class formKujundid
-    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+    Private Sub tootleKujund(ByRef kujund As Kujund)
+        'kujundi tyybi valimine'
+        lblTyyp.Text = kujund.annaTyyp
 
+        'pindala ja ymbermoot arvutamine ja väljastamine
+        txtPindala.Text = kujund.leiaPindala
+        txtYmbermoot.Text = kujund.leiaYmbermoot
     End Sub
 
-    Private Sub Label8_Click(sender As Object, e As EventArgs) Handles Label8.Click
+    Private Sub btnKolmnurk_Click(sender As Object, e As EventArgs) Handles btnKolmnurk.Click
+        'objekti kolmnurga klassist'
+        Dim objektKolmnurk As New Kolmnurk(txtKylgA.Text, txtKylgB.Text)
+        tootleKujund(objektKolmnurk)
+    End Sub
 
+    Private Sub btnRuut_Click(sender As Object, e As EventArgs) Handles btnRuut.Click
+        Dim objektRuut As New Ruut(txtKylgA.Text)
+        tootleKujund(objektRuut)
+    End Sub
+
+    Private Sub btnRistkylik_Click(sender As Object, e As EventArgs) Handles btnRistkylik.Click
+        'võib teha sama asja ka nii...'
+        tootleKujund(New Ristkylik(txtKylgA.Text, txtKylgB.Text))
     End Sub
 End Class
