9c126f46c4ffebd09375ed0ff4362cdc285b1b99
Karina Reisel <kreise@taltech.ee>
2024-02-28 17:36:12 +0200

    juhendi jrgi valmis. teha yl 1 ja yl2

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.PrjKujundid.Form1
+            Me.MainForm = Global.PrjKujundid.formKujundid
         End Sub
     End Class
 End Namespace
