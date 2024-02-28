941bfc2ff9d8892855dc809b66ceaf40cb0d11a8
Karina Reisel <kreise@taltech.ee>
2024-02-28 11:52:26 +0200

    kolmnurk

---
+++ Kolmnurk.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Math
+Public Class Kolmnurk
+    Inherits Kujund
+
+    Public Overrides Function leiaYmberMoot() As Double
+        Throw New NotImplementedException()
+    End Function
+
+    Public Overrides Function leiaPindala() As Double
+        Throw New NotImplementedException()
+    End Function
+End Class
